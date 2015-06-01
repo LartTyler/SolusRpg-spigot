33a94366a0c0a59041f76e29f2f0d83b5a671964
Tyler Lartonoix <larttyler@live.com>
2015-06-01 16:22:55 -0400

    Began adding language system.

--- src/main/java/me/dbstudios/solusrpg/configuration/Language.java
+++ src/main/java/me/dbstudios/solusrpg/configuration/Language.java
@@ -4,7 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Language {
-	private static final Map<String, String> phrases = new HashMap<>();
+	private static final Map<String, String> languages = new HashSet<>();
+	// private static final Map<String, String> phrases = new HashMap<>();
 
+	static {
+		Language.init();
+	}
 
+	private static void init() {
+		File f = new File(Directories.CONFIG_LANG + "registry.yml");
+
+		if (!f.exists())
+			throw new RuntimeException("No language registry found. SolusRpg cannot function without a language file.");
+
+		FileConfiguration registry = YamlConfiguration.loadConfiguration(f);
+		ConfigurationSection language = registry.getConfigurationSection("language");
+
+		if (language != null)
+			for (String key : language.getKeys(false))
+				registry.put(key, language.getString(key, key));
+		else
+			throw new RuntimeException("No language files exist in the registry. SolusRpg cannot function without a language file.");
+	}
 }
